7d059ee83c515b3d3789cb4fa206b06341d9ce6a
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2022-08-30 23:05:05 +0500

    fixed mul operator

--- matrix mul.py
+++ matrix mul.py
@@ -1,3 +1,26 @@
+from collections.abc import Sequence
+
+
+def get_shape(lst, shape=()):
+    if not isinstance(lst, Sequence):
+        # base case
+        return shape
+
+    # peek ahead and assure all lists in the next depth
+    # have the same length
+    if isinstance(lst[0], Sequence):
+        l = len(lst[0])
+        if not all(len(item) == l for item in lst):
+            msg = 'not all lists have the same length'
+            raise ValueError(msg)
+
+    shape += (len(lst), )
+    
+    # recurse
+    shape = get_shape(lst[0], shape)
+
+    return shape
+
 # it doenst work for now
 def is2DList(matrix_list):
   if isinstance(matrix_list[0], list):
@@ -9,14 +32,14 @@ def mulArray(oneDArray , nDArray):
     if  not is2DList(nDArray):
         res = 0
         for index , value in enumerate(oneDArray):
-           res = res + oneDArray[index] + nDArray[index]
+           res = res + oneDArray[index] * nDArray[index]
         return res
     else:
         if (len(nDArray[0]) == len(oneDArray)):
             res = 0
             for index , value in enumerate(oneDArray):
                 for index2 , value2 in enumerate(nDArray[index]):
-                    res = res + value + value2
+                    res = res + value * value2
             return res
 
 l1 = [[1,2,3],[1,2,3]]
